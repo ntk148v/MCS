ec1f2970b1e07a84830b76b0a6b0d1c70bac00cf
Kien Nguyen <ntk148v@gmail.com>
2017-03-20 09:01:42 +0700

    Add 2 models

--- mcs/apps/dashboard/models.py
+++ mcs/apps/dashboard/models.py
@@ -2,4 +2,26 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class CloudRing(models.Model):
+
+    class Meta:
+        db_table = 'cloudring'
+        app_label = 'dashboard'
+
+    identifier = models.CharField(max_length=50)
+    USERNAME_FIELD = 'identifier'
+
+
+class CloudNode(models.Model):
+
+    class Meta:
+        db_table = 'cloudnode'
+        app_label = 'dashboard'
+
+    identifier = models.CharField(max_length=50)
+    config = models.TextField()
+    type = models.TextField()
+    ring = models.ForeignKey(CloudRing, on_delete=models.CASCADE)
+
+    USERNAME_FIELD = 'identifier'
